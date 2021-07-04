1b72cc9220accf19875145d2555f103afb675ce1
MicroboyT-T <muazsolihin11@gmail.com>
2021-07-04 09:09:03 +0800

    Window interface commit

--- group_p/client.py
+++ group_p/client.py
@@ -8,7 +8,7 @@ height = 700
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-
+#Button interface part#
 class Button:
     def __init__(self, text, x, y, color):
         self.text = text
@@ -31,3 +31,52 @@ class Button:
             return True
         else:
             return False
+
+
+#Window part#
+def redrawWindow(win, game, p):
+    win.fill((0,0,0))
+
+    if not(game.connected()):
+        font = pygame.font.SysFont("comicsans", 65)
+        text = font.render("Menunggu Pemain Lain...", 1, (255,0,0), True)
+        win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+    else:
+        font = pygame.font.SysFont("comicsans", 60)
+        text = font.render("Anda", 1, (0, 0,255))
+        win.blit(text, (180, 200))
+
+        text = font.render("Lawan", 1, (255, 0, 0))
+        win.blit(text, (380, 200))
+
+        move1 = game.get_player_move(0)
+        move2 = game.get_player_move(1)
+        if game.bothWent():
+            text1 = font.render(move1, 1, (255,255,255))
+            text2 = font.render(move2, 1, (255,255,255))
+        else:
+            if game.p1Went and p == 0:
+                text1 = font.render(move1, 1, (255,255,255))
+            elif game.p1Went:
+                text1 = font.render("Sudah Pilih", 1, (255,255,255))
+            else:
+                text1 = font.render("Menunggu..", 1, (255,255,255))
+
+            if game.p2Went and p == 1:
+                text2 = font.render(move2, 1, (255,255,255))
+            elif game.p2Went:
+                text2 = font.render("Sudah Pilih", 1, (255,255,255))
+            else:
+                text2 = font.render("Menunggu..", 1, (255,255,255))
+
+        if p == 1:
+            win.blit(text2, (100, 350))
+            win.blit(text1, (380, 350))
+        else:
+            win.blit(text1, (100, 350))
+            win.blit(text2, (380, 350))
+
+        for btn in btns:
+            btn.draw(win)
+
+    pygame.display.update()
