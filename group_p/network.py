c369a0504f1828b909e4783ee68f14cc4c431133
alipsho <alifimran164@gmail.com>
2021-07-04 09:28:31 +0800

    network library for client

---
+++ group_p/network.py
@@ -0,0 +1,28 @@
+import socket
+import pickle
+
+
+class Network:
+    def _init_(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.56.1"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048*2))
+        except socket.error as e:
+            print(e)
