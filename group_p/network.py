de054500a66538b75b36ab37ea41d16e8ec7cf60
MicroboyT-T <muazsolihin11@gmail.com>
2021-07-04 10:57:06 +0800

    Add a few comment for network

--- group_p/network.py
+++ group_p/network.py
@@ -5,8 +5,8 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.56.1"
-        self.port = 5555
+        self.server = "192.168.56.1" #must sync with IP at the server#
+        self.port = 5555             #must have a same port at the server#
         self.addr = (self.server, self.port)
         self.p = self.connect()
 
